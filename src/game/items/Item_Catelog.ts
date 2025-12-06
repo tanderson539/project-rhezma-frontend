1fd096aeb406b160a69f80ac42ed8a11b7297eb6
tanner <tanderson539@gmail.com>
2025-12-05 20:47:38 -0700

    [feat] - Adds items. Specifically types the ITEM_CATELOG. Specifically types items. Adds raw_material item category.

--- src/game/items/Item_Catelog.ts
+++ src/game/items/Item_Catelog.ts
@@ -1,16 +1,20 @@
-export const ITEM_CATALOG = {
-    WOOD_LOG: { name: 'Wood Log', stackable: true, category: 'material' },
-    OAK_LOG: { name: 'Oak Log', stackable: true, category: 'material' },
+export const ITEM_CATALOG: Readonly<Record<string, Item>> = {
+    WOOD_LOG: { name: 'Wood Log', category: 'material' },
+    OAK_LOG: { name: 'Oak Log', category: 'material' },
     STONE_UNREFINED: {
         name: 'Unrefined Stone',
-        stackable: true,
         category: 'material',
     },
-    STONE_BRICK: { name: 'Stone Brick', stackable: true, category: 'material' },
-    IRON_ORE: { name: 'Iron Ore', stackable: true, category: 'material' },
-    IRON_BAR: { name: 'Iron Bar', stackable: true, category: 'material' },
-    GOLD_COIN: { name: 'Gold Coin', stackable: true, category: 'currency' },
+    STONE_BRICK: { name: 'Stone Brick', category: 'material' },
+    IRON_ORE: { name: 'Iron Ore', category: 'material' },
+    IRON_BAR: { name: 'Iron Bar', category: 'material' },
+    GOLD_COIN: { name: 'Gold Coin', category: 'currency' },
 } as const;
+
+export interface Item {
+    name: string;
+    category: 'material' | 'currency';
+}
 
 export type ItemKey = keyof typeof ITEM_CATALOG;
 
